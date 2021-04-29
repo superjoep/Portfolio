9b9886da77599af6a538e7a526611030a3ef7827
Joep <joepdekock@gmail.com>
2021-04-29 10:58:56 +0200

    Header in include

---
+++ Index.php
@@ -0,0 +1,10 @@
+<?php 
+include 'Includes/Header.php';
+?>
+<link rel="stylesheet" href="Index.css">
+
+
+<body style="background-color: #5CDB95;">
+  <img src="Images/DrawingMyself.png" id="HomeImage">
+  <h1 id="titletext">Joep</br>de</br>Kock.</br></h1>
+</body>
